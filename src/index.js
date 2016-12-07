a3d1e2162aa02144346c8413f480f2dc441ace80
Sarah Rose Battles <srosebattles@gmail.com>
2016-12-07 14:53:05 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {Router, Route, Link, browserHistory} from 'react-router'
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
